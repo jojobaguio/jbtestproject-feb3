79dfd333f0775cb06a7cbd1d76be418171539ea5
jbags0612 <jbbaguio0612@gmail.com>
2018-02-03 13:52:45 +0800

    test commit only

--- findall_href.py
+++ findall_href.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import urllib                                       
 
+# test only
 def test_findallhref():
 	soup = BeautifulSoup(urllib.urlopen("https://winbet.cc/index.php?m=index").read(), "lxml")
 	for link in soup.find_all('a'):
