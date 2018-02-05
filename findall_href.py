b9c6f54db1691aee7a88453117f1ee8f642eccf3
jbags0612 <jbbaguio0612@gmail.com>
2018-02-05 14:54:39 +0800

    updates from local branch

--- findall_href.py
+++ findall_href.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import urllib                                       
 
-# test only
+# test only by jojo from local branch
 # test again Feb3
 def test_findallhref():
 	soup = BeautifulSoup(urllib.urlopen("https://winbet.cc/index.php?m=index").read(), "lxml")
